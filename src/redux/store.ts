c2e66a5362151b0ca6dc9510bbad598b20244438
TH-Raju <rajukhan894200@gmail.com>
2023-08-25 11:42:33 +0600

    [Done] redux user api set

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { api } from './api/apiSlice';
 import bookReducer from './features/Books/BookSlice';
+import userReducer from './features/user/userSlice';
 import usewishListReducer from './features/wishlist/whishListSlice';
 
 const store = configureStore({
 
     reducer: {
 
+        user: userReducer,
         book: bookReducer,
         wishlist: usewishListReducer,
         [api.reducerPath]: api.reducer
