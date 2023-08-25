d31b1062580c86859664d67ce2b3021be636cddf
TH-Raju <rajukhan894200@gmail.com>
2023-08-25 11:24:00 +0600

    [Done] book typeinterface

---
+++ src/types/BooksType.ts
@@ -0,0 +1,12 @@
+export interface IBook {
+        _id:string |null,
+        Title: string |null,
+        Author: string |null,
+        Genre: string |null,
+        PublicationDate:  |null,
+        Reviews: string[] |[],
+        status?:boolean | string |null,
+        readingStatus?:string |null
+        wishlistStatus?:boolean
+        email?:string
+      }
