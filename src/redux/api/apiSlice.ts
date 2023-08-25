17cafcdd98f774d3b193c8c055f8a0358f713873
TH-Raju <rajukhan894200@gmail.com>
2023-08-25 15:00:19 +0600

    [Done] server link added

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://book-catalog-backend-ecru.vercel.app/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://book-catalog-backend-tawny.vercel.app/' }),
     tagTypes: ['books', 'reviews', 'editBooks', 'postbooks', 'postwishlist', 'postreadinglist', 'updateStatus'],
     endpoints: () => ({}),
 });
