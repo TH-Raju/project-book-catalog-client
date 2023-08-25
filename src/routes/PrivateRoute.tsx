b44b64d6c4d171435098d575731619a5ce2666e4
TH-Raju <rajukhan894200@gmail.com>
2023-08-25 12:01:18 +0600

    [Done] private route setup

---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,26 @@
+import { useAppSelector } from '@/redux/hooks';
+import { ReactNode } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+// RingLoader
+interface IProps {
+    children: ReactNode;
+  }
+
+
+export default function PrivateRoute({children}:IProps) {
+    const {pathname}= useLocation()
+
+      
+    const {user,isLoading} =  useAppSelector((state:any)=>state.user)
+    console.log(isLoading)
+    if(isLoading){
+     return   <p>loading........</p>
+    }
+    if (!user.email && !isLoading) {
+      return <Navigate to="/login" state={{ path: pathname }} />;
+    }
+  
+
+  
+    return children;
+}
